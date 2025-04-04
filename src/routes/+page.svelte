12499f331d3a2d2563b05581fc3b5f64bc77e569
BitFlippy <bitflippy@proton.me>
2025-04-04 16:48:18 +0200

    chore(init): Add barebones scaffolding of the website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let { data } = $props();
+</script>
+
+<svelte:head>
+    <title>{data.seo.title}</title>
+    <meta name="description" content={data.seo.description} />
+    <meta property="og:image" content={data.seo.ogImage} />
+</svelte:head>
+
+
+<h1>Hello, World!</h1>
---
+++ src/routes/+page.ts
@@ -0,0 +1,11 @@
+import type {PageLoad} from './$types';
+
+export const load: PageLoad = async () => {
+    return {
+        seo: {
+            title: 'Polly — Wife of Pax',
+            description: 'The official site of Polly, Pax’s beloved companion and character in the Pudgy Penguins universe.',
+            ogImage: 'https://pollyabstract.xyz/og-image.png',
+        },
+    };
+};
