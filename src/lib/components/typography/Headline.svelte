eee28dd66a56a5045ae5716d54875212783b3ff5
BitFlippy <bitflippy@proton.me>
2025-04-06 12:08:51 +0200

    chore: Remove unused imports

--- src/lib/components/typography/Headline.svelte
+++ src/lib/components/typography/Headline.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
-  import type { ClassValue, HTMLAttributes } from 'svelte/elements';
+  import type { HTMLAttributes } from 'svelte/elements';
 
   interface Props extends HTMLAttributes<HTMLHeadingElement> {
     children: Snippet;
